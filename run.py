4ada032dd9125194c63e41e88695140aba9f2327
EvanSmith2002 <evanasmi@usc.edu>
2024-11-15 11:31:38 -0800

    Initial Commit

--- run.py
+++ run.py
@@ -5,39 +5,53 @@ from scipy.stats import zscore
 
 # Question 1: Pre-process the data
 def preprocess_data(input_filename):
-    pass
-
+    data = pd.read_csv(input_filename)
+    data.columns = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"]
+    z_score_length = (data["SepalLengthCm"] - data["SepalLengthCm"].mean()) / data["SepalLengthCm"].std()
+    z_score_width = (data["SepalWidthCm"] - data["SepalWidthCm"].mean()) / data["SepalWidthCm"].std()
+    data = data[(abs(z_score_length) <= 2) & (abs(z_score_width) <= 2)]
+    data["ID"] = range(1, len(data) + 1)
+    return data
 
 
 # Question 2: Descriptive Statistics Functions
 def species_count(data):
-    pass 
+    df = preprocess_data(data)
+    return df["Species"].value_counts()
 
 def average_sepal_length(data):
-    pass
+    df = preprocess_data(data)
+    return round(df["SepalLengthCm"].mean(), 1)
 
 def max_petal_width(data):
-    pass
+    df = preprocess_data(data)
+    return round(df["PetalWidthCm"].max(), 1)
 
 def min_petal_length(data):
-    pass
+    df = preprocess_data(data)
+    return round(df["PetalLengthCm"].min(), 1)
 
 def count_sepal_length_above_5(data):
-    pass
+    df = preprocess_data(data)
+    return len(df[df["SepalLengthCm"] > 5])
 
 # Question 3: Analysis Functions
 def count_petal_length_below_2(data):
-    pass
+    df = preprocess_data(data)
+    return len(df[df["PetalLengthCm"] < 2])
 
 def get_sepal_width_above_3_5(data):
-    pass
+    df = preprocess_data(data)
+    return list(df[df["SepalWidthCm"] > 3.5]["ID"].values)
 
 def species_count_petal_width_above_1_5(data):
-    pass
+    df = preprocess_data(data)
+    return df[df["PetalWidthCm"] > 1.5].value_counts()
 
 def get_virginica_petal_length_above_6(data):
-    pass
+    df = preprocess_data(data)
+    return list(df[(df["PetalLengthCm"] > 6) & (df["Species"] == "Iris-virginica")]["ID"].values)
     
 def get_largest_sepal_width(data):
-    pass
-
+    df = preprocess_data(data)
+    return df[df["SepalWidthCm"] == df["SepalWidthCm"].max()]["ID"]
